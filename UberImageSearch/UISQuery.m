ff4bb5914370d0d5aa30021cebd4e747e773c011
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-05-05 14:53:29 -0700

    atvalidations + small bug fix

--- UberImageSearch/UISQuery.m
+++ UberImageSearch/UISQuery.m
@@ -10,6 +10,8 @@
 
 #import <AFNetworking/AFNetworking.h>
 
+#import <ATValidations/ATValidations.h>
+
 NSString *const kUISGoogleImageSearchURL = @"https://ajax.googleapis.com/ajax/services/search/images";
 
 
@@ -24,6 +26,40 @@ NSString *const kUISGoogleImageSearchURL = @"https://ajax.googleapis.com/ajax/se
 @property (readwrite) BOOL isLoading;
 
 @end
+
+
+
+ATVPredicate *UISGoogleImageSearchResponseValidation(void);
+ATVPredicate *UISGoogleImageSearchResponseValidation() {
+    static ATVPredicate *instance = nil;
+    if (!instance) {
+        // The first argument is a key-predicate mapping of what we expect the data to look like.
+        // The second argument (BOOL) specifies whether extra non-specified keys in the data is considered an error. Here, we allow it.
+        // Essentially, we're only validating the parts of the response that we're using. If Google were to add a new response key, it would not break this client.
+        instance = ATVDictionary(
+                    @{
+                      @"responseData": ATVDictionary(
+                        @{
+                          @"cursor": ATVDictionary(
+                            @{
+                              @"currentPageIndex": ATVNumber(),
+                              @"pages": ATVArrayOf(ATVDictionary(@{@"start": ATVString()}, YES))
+                              }, YES),
+                          @"results": ATVArrayOf(ATVDictionary(
+                            @{
+                              @"tbUrl": ATVMatchesBlock(^BOOL(id object, NSError *__autoreleasing *error) {
+                                    // Test that this is a valid URL by testing whether we can successfully create a NSURL
+                                    if (!ATVAssert([object isKindOfClass:[NSString class]], @"must be a String representation of a URL", error)) return NO;
+                                    NSURL *url = [NSURL URLWithString:object];
+                                    return ATVAssert(url && url.scheme && url.host, @"must be a String representation of a URL", error);
+                                })
+                              }, YES))
+                          }, YES)
+                      }, YES);
+    }
+    return instance;
+}
+
 
 
 @implementation UISQuery
@@ -50,7 +86,7 @@ NSString *const kUISGoogleImageSearchURL = @"https://ajax.googleapis.com/ajax/se
     // note sure what is up here; api will not return enough pages?
     if ([pages count] <= nextPage) return;
     
-    NSInteger nextStart = ((NSNumber *)pages[nextPage][@"start"]).integerValue;
+    NSInteger nextStart = ((NSString *)pages[nextPage][@"start"]).integerValue;
 
     [self performSearchStart:nextStart forQuery:self.query];
 }
@@ -66,7 +102,16 @@ NSString *const kUISGoogleImageSearchURL = @"https://ajax.googleapis.com/ajax/se
     [manager GET:kUISGoogleImageSearchURL
       parameters:@{@"v": @"1.0", @"rsz": @8, @"q": queryString, @"start": @(start)}
          success:^(__unused AFHTTPRequestOperation *operation, NSDictionary *responseObject) {
-             [self handleSearchResponse:(NSDictionary *)responseObject];
+             
+             NSError *error;
+             if ([responseObject matchesATVPredicate:UISGoogleImageSearchResponseValidation() error:&error]) {
+                 [self handleSearchResponse:(NSDictionary *)responseObject];
+             } else {
+                 NSLog(@"API response did not match expected pattern: %@", error);
+                 self.isLoading = NO;
+             }
+             
+             
          } failure:^(__unused AFHTTPRequestOperation *operation, NSError *error) {
              NSLog(@"Error loading google image search: %@", error);
              // TODO: show a real alert view, or something
